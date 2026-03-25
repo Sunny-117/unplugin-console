dd9fc7f732f458b1dbc1af34caf8b29c192b0b5a
Sunny-117 <zhiqiangfu6@gmail.com>
2026-03-25 15:48:33 +0800

    chore: fix

--- playground/vite/vite.config.ts
+++ playground/vite/vite.config.ts
@@ -1,14 +1,8 @@
-import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import Unplugin from 'unplugin-console/vite'
 
 export default defineConfig({
-  server: {
-    fs: {
-      allow: [resolve(import.meta.dirname, '..')],
-    },
-  },
   plugins: [
     Inspect(),
     Unplugin({
